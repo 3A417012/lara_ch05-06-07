6495ba3c29e980918cc781f2e50922bf5739cf86
3A417012 <nds10745@gmail.com>
2018-10-30 16:31:17 +0800

    Exc5 - Edit normalization string using Route::pattern

--- routes/web.php
+++ routes/web.php
@@ -36,11 +36,13 @@ Route::get('student/{student_no}/score/{subject?}', function ($student_no,$subje
     return "學號 ".$student_no." 的 ".((is_null($subject))?"所有科目":$subject)." 成績";
 });
 */
+Route::pattern('student_no','3A[0-9]{6}');
+Route::pattern('subject','(國文|英文|數學|歷史|通識)');
 
 Route::get('student/{student_no}', function ($student_no) {
     return "學號：".$student_no;
-})->where(['student_no'=>'3A[0-9]{6}']);
+});
 
 Route::get('student/{student_no}/score/{subject?}', function ($student_no,$subject = null) {
     return "學號 ".$student_no." 的 ".((is_null($subject))?"所有科目":$subject)." 成績";
-})->where(['student_no'=>'3A[0-9]{6}','subject'=>'(國文|英文|數學|歷史|通識)']);
+});
